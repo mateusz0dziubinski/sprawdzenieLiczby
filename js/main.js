e81ab8eaf7fde8525dc6b4413e40e03863211cdb
mateusz0dziubinski <mateusz0dziubinski@o2.pl>
2019-09-10 21:22:43 +0200

    Zadanie: zrobione

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var wpiszliczbe = 150;
 var liczba = wpiszliczbe;
 
-function wynik(wpiszliczbe){
+function wynik(){
     if (liczba >= 100 && liczba <= 200){
         console.log("Liczba znajduję się w przedziale od 100 do 200")
     }else{
